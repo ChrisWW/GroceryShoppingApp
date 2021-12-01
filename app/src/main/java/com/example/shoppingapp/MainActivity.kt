56f7c8dfef0c09257fb337d2c52b7efb78f0723d
Krystian Wiewior <krystian.wiewior@gmail.com>
2021-12-01 23:17:32 +0100

    room, repo, viewmodel, 2nd fragment added

---
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.shoppingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+//    lateinit var itemsRV: RecyclerView
+//    lateinit var addFAB: FloatingActionButton
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
